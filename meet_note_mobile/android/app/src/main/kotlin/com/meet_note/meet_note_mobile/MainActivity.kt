29619da29d8284a221f54dde01dda58995d5dbf8
Stefan <ste.schaller@gmx.de>
2019-05-30 08:53:14 +0200

    Make Toolbar transparent

--- meet_note_mobile/android/app/src/main/kotlin/com/meet_note/meet_note_mobile/MainActivity.kt
+++ meet_note_mobile/android/app/src/main/kotlin/com/meet_note/meet_note_mobile/MainActivity.kt
@@ -3,11 +3,16 @@ package com.meet_note.meet_note_mobile
 import android.os.Bundle
 
 import io.flutter.app.FlutterActivity
+import android.graphics.Color
+import android.os.Build
 import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     GeneratedPluginRegistrant.registerWith(this)
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+      window.statusBarColor = Color.TRANSPARENT
+    }
   }
 }
