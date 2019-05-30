aa9c9b6022c59feb33c7ff05b122b11bc35be51c
Stefan <ste.schaller@gmx.de>
2019-05-30 12:30:08 +0200

    feat/implementation mock impl service

--- meet_note_core/lib/service/task_service.dart
+++ meet_note_core/lib/service/task_service.dart
@@ -1,16 +1,38 @@
+import 'dart:async';
+
 import 'package:meet_note_core/models/task.dart';
+import 'package:uuid/uuid.dart';
 
 abstract class TaskService {
-  Future<List<Task>> getAll();
+  Stream<List<Task>> getAll();
 
   Future<bool> delete(String id);
+
+  Future<bool> update(Task task);
 
   Future<void> create(String title);
 }
 
 class TaskServiceImpl implements TaskService {
+  List<Task> _taskList = List();
+
+  StreamController<Task> sdsd;
+
   @override
   Future<void> create(String title) {
+    Task task = Task(
+      id: Uuid().v4(),
+      title: title,
+      done: false,
+    );
+
+    _taskList.add(task);
+
+    return Future.value();
+  }
+
+  @override
+  Future<bool> update(Task task) {
     return Future.value();
   }
 
@@ -20,8 +42,8 @@ class TaskServiceImpl implements TaskService {
   }
 
   @override
-  Future<List<Task>> getAll() {
-    return Future.value([
+  Stream<List<Task>> getAll() {
+    return Stream.fromFuture(Future.value([
       Task(title: "ddd", id: "uuid", done: true),
       Task(title: "gggf", id: "uuid", done: false),
       Task(title: "asds", id: "uuid", done: true),
@@ -29,6 +51,6 @@ class TaskServiceImpl implements TaskService {
       Task(title: "dfhh", id: "uuid", done: false),
       Task(title: "jjjhj", id: "uuid", done: true),
       Task(title: "hhzh", id: "uuid", done: false)
-    ]);
+    ]));
   }
 }
