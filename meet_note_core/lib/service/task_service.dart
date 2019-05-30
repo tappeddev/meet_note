c5380d286afc9f2b2ccfd2c1d6333674e5376add
Stefan <ste.schaller@gmx.de>
2019-05-30 11:37:43 +0200

    feat/implementation impl vm and state

--- meet_note_core/lib/service/task_service.dart
+++ meet_note_core/lib/service/task_service.dart
@@ -1,9 +1,32 @@
 
+import 'package:meet_note_core/models/task.dart';
+
 abstract class TaskService {
 
-  Future<List<TaskService>> getAll();
+  Future<List<Task>> getAll();
 
   Future<bool> delete(String id);
 
   Future<void> create(String title);
+}
+
+class TaskServiceImpl implements TaskService {
+
+  @override
+  Future<void> create(String title) {
+    // TODO: implement create
+    return null;
+  }
+
+  @override
+  Future<bool> delete(String id) {
+    // TODO: implement delete
+    return null;
+  }
+
+  @override
+  Future<List<Task>> getAll() {
+    // TODO: implement getAll
+    return null;
+  }
 }
--- meet_note_core/lib/view_model/task_list_view_model.dart
+++ meet_note_core/lib/view_model/task_list_view_model.dart
@@ -1,33 +1,57 @@
 import 'package:meet_note_core/models/task.dart';
 import 'package:meet_note_core/service/task_service.dart';
+import 'package:meta/meta.dart';
 import 'package:stateful_view_model/stateful_view_model.dart';
 
 class TaskListState implements Cloneable<TaskListState> {
-  String currentYear;
-  String currentDay;
+  String year;
+  String day;
+  String month;
 
   List<Task> taskList;
 
+  TaskListState(
+      {@required this.day,
+      @required this.month,
+      @required this.year,
+      @required this.taskList});
+
   @override
-  TaskListState copy() {
-    // TODO: implement copy
-    return null;
-  }
+  TaskListState copy() => TaskListState(
+      day: day, month: month, year: year, taskList: List.of(taskList));
 }
 
 class TaskListViewModel extends StatefulViewModel<TaskListState> {
   final TaskService _taskService;
 
   TaskListViewModel(this._taskService, TaskListState initialState)
-      : super(initialState);
+      : super(initialState) {
+    final now = DateTime.now();
 
-  
-  void createTask(String title){
+    setState((state) {
+      state.year = now.year.toString();
+      state.month = now.month.toString();
+      state.day = now.day.toString();
+      return state;
+    });
 
-    //TODO
-
+    _taskService.getAll().then(_handleFetchedList);
   }
 
+  void createTask(String title) {
+    _taskService.create(title).then((_) {
+      _taskService.getAll().then(_handleFetchedList);
+    });
+  }
+
+  // ------
+  // Helper
+  // ------
+
+  void _handleFetchedList(List<Task> taskList) {
+    setState((state) {
+      state.taskList = taskList;
+      return state;
+    });
+  }
 }
-
-
